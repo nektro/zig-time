8b1216b2ddaf9fbab12d5b686f142f0c99d472d1
Meghan Denny <hello@nektro.net>
2023-05-16 11:19:02 -0700

    update build.zig for zig master

--- build.zig
+++ build.zig
@@ -2,9 +2,13 @@ const std = @import("std");
 const deps = @import("./deps.zig");
 
 pub fn build(b: *std.build.Builder) void {
-    const t = b.addTest("main.zig");
+    const t = b.addTest(.{
+        .root_source_file = .{ .path = "main.zig" },
+    });
     deps.addAllTo(t);
 
+    const run_t = b.addRunArtifact(t);
+
     const t_step = b.step("run", "Run all the tests.");
-    t_step.dependOn(&t.step);
+    t_step.dependOn(&run_t.step);
 }
