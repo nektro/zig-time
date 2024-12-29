d96fd09fb721265373e5f48a7d017f776cea0415
Meghan Denny <hello@nektro.net>
2024-12-28 19:33:37 -0800

    update to zig 0.13

--- build.zig
+++ build.zig
@@ -2,12 +2,18 @@ const std = @import("std");
 const deps = @import("./deps.zig");
 
 pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const mode = b.option(std.builtin.Mode, "mode", "") orelse .Debug;
+
     const t = b.addTest(.{
-        .root_source_file = .{ .path = "main.zig" },
+        .root_source_file = b.path("main.zig"),
+        .target = target,
+        .optimize = mode,
     });
     deps.addAllTo(t);
 
     const run_t = b.addRunArtifact(t);
+    run_t.has_side_effects = true;
 
     const t_step = b.step("test", "Run all the tests.");
     t_step.dependOn(&run_t.step);
